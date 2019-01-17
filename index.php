d53b7e42b731bc54ab60d1f03f06848e08b33702
ValentinGor <v.gorbatenko@mail.ru>
2019-01-17 18:58:53 +0200

    layout first screen

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
     <!-- Fonts CSS -->
+    <link rel="stylesheet" href="fonts/fonts.css">
     <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">
 
     <!-- Owlcarousel CSS -->
@@ -32,7 +33,60 @@
     <title>Hello, world!</title>
 </head>
 <body>
-<h1>Hello, world!</h1>
+<div class="main__section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="contain-breadcrumb">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb aim-breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Blog </a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Business analytics</li>
+                        </ol>
+                    </nav>
+                </div>
+                <div class="contain-slogan">
+                    <div class="slogan">
+                        WHO IS REQUIRED TO SUBMIT CONTROLLED TRANSACTIONS REPORT AND <br>PREPARE TRANSFER PRICING DOCUMENTATION?
+                    </div>
+                </div>
+                <div class="contain-paternity">
+                    <div class="paternity">
+                        <div class="paternity__author">
+                            <div class="paternity__author_img"><img src="img/author.png" alt="img"></div>
+                            <div class="paternity__author_text">Author:</div>
+                            <div class="paternity__author_name">Oleksandra Konopatska</div>
+                        </div>
+                        <div class="paternity__reading">
+                            <div class="paternity__reading_icon"><img src="img/clock.svg" alt="ico"></div>
+                            <div class="paternity__reading_text">Reading time:</div>
+                            <div class="paternity__reading_time">8 min.</div>
+                        </div>
+                        <div class="paternity__views">
+                            <div class="paternity__views_icon"><img src="img/visibility.svg" alt="ico"></div>
+                            <div class="paternity__views_text">Views:</div>
+                            <div class="paternity__views_info">8 min.</div>
+                        </div>
+                        <div class="paternity__star">
+                            <div class="paternity__star_img"><img src="img/star-w.svg" alt="ico"></div>
+                            <div class="paternity__star_img"><img src="img/star-w.svg" alt="ico"></div>
+                            <div class="paternity__star_img"><img src="img/star-w.svg" alt="ico"></div>
+                            <div class="paternity__star_img"><img src="img/star-w.svg" alt="ico"></div>
+                            <div class="paternity__star_img"><img src="img/star-g.svg" alt="ico"></div>
+                        </div>
+                    </div>
+                    <div class="date">
+                        <div class="date_day">02</div>
+                        <div class="contain_m">
+                            <div class="date_month">June</div>
+                            <div class="date_year">2018</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="js/jquery-3.3.1.min.js"></script>
