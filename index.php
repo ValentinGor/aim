30bb3f37dcd1063d8d135ab9b11d009ff183fd8b
ValentinGor <v.gorbatenko@mail.ru>
2019-01-18 14:18:54 +0200

    block with the article is laid out (rough markup without adaptations)

--- index.php
+++ index.php
@@ -87,7 +87,285 @@
         </div>
     </div>
 </div>
+<div class="gen__section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="contain-articles">
+                    <div class="articles">
+                        <p>
+                            If you are having an urgent need to prepare transfer pricing documentation, you have to be ready for a time-consuming process. The amount and
+                            complexity of work associated with it can easily turn this task into a real <span>pain in the neck.</span>
+                        </p>
+                        <p>
+                            However, first you have to figure out whether you actually fall within the scope of the Article 39 or Paragraph 140.5.4 of the of the Tax Code of
+                            Ukraine and whether transfer pricing is something you have to deal with in the first place.
+                        </p>
+                        <h2>
+                            You are expected to submit transfer pricing documentation, if your transactions are recognized as controlled.
+                        </h2>
+                        <p>
+                            The very definition of controlled transactions has its hidden pitfalls, which is why it is hard to tackle this matter right away, especially
+                            against the backdrop of the ever-changing terms of the tax legislation.
+                        </p>
+                        <p>
+                            In particular, in 2017-2018, changes were made both to the threshold amount of transactions involving counterparty and the requirements as to the
+                            taxpayer’s minimum net profit. Aside from that, some of the criteria for recognizing transactions as controlled were extended.
+                        </p>
+                        <img class="img-fluid" src="img/img_1.png" alt="img">
+                        <h3>
+                            Let's start off with the latter and try figure out when transactions are not deemed controlled.
+                        </h3>
+                        <p>
+                            It stands to mention that the sharp eye of the State Fiscal <span>Service</span> has zero interest in "small" transactions: for the transaction to
+                            be deemed
+                            controlled, the <strong>sum of transactions involving one counterparty</strong> (by direct agreements or through intermediaries that perform no
+                            major functions
+                            within the framework of the agreement) shall start from <strong>UAH 10 million</strong> in 2017-2018 (or UAH 5 million in 2015-2016).
+                        </p>
+                        <p>
+                            An additional criterion for recognizing transactions as controlled is the total amount of taxpayer's income resulting from all types of activities.
+                            In 2015-2016, the threshold amount was <strong>UAH 50 million</strong> and has been increased to UAH
+                        </p>
+                        <h3>
+                            Companies having smaller revenues cannot have controlled transactions.
+                        </h3>
+                        <p>
+                            <strong>The counterparty</strong> is another crucial aspect when it comes to controlled transaction. Below is a list of criteria, according to
+                            which the transaction
+                            is recognized as controlled in 2018, obviously in case of the relevant sum of transactions and total amount of taxpayer’s income.
+                        </p>
+                        <ul>
+                            <li><span>Counterparty is an affiliated person</span></li>
+                            <li><span>Counterparty is a commission agent</span></li>
+                            <li><span>Counterparty is located in the country with low profit tax</span></li>
+                            <li><span>Counterparty’s legal form of organization exempts it from profit tax</span></li>
+                        </ul>
 
+                        <p>
+                            <span class="imitation-h3">Please note! </span> The list of given criteria is for the year 2018. The terms and conditions with respect to the
+                            previous years may differ.
+                        </p>
+                        <h2>
+                            Transfer pricing: the price of the violation
+                        </h2>
+                        <img class="img-fluid" src="img/img_2.png" alt="img">
+                        <h4>
+                            So, let’s sum up.
+                        </h4>
+                        <p>
+                            Your transaction is not deemed controlled, if at least one of the following criteria is met:
+                        </p>
+                        <ul>
+                            <li class="point-circle"><span>The total amount of taxpayer's income for the year does not exceed UAH 150 million; </span></li>
+                            <li class="point-circle"><span>Volume of transaction involving a non-resident does not exceed UAH 10 million;</span></li>
+                            <li class="point-circle"><span>The counterparty does not meet any of the options given in the table above.</span></li>
+                        </ul>
+                        <h5>
+                            However, even if your transaction is not controlled, it does not mean that you will avoid having to prepare transfer pricing documentation
+                            altogether.
+                        </h5>
+                        <p>
+                            Pursuant to Paragraph 140.5.4 of the Tax Code of Ukraine, you are required to submit documentation regarding transactions which are not recognized
+                            as controlled, should they involve counterparties:
+                        </p>
+                        <ul>
+                            <li class="point-circle">
+                                <span>based in the countries with low profit tax (from the relevant list approved by the Cabinet of Ministers of Ukraine);</span></li>
+                            <li class="point-circle"><span>that have legal form of organization which implies payment of profit tax in a country where the counterparty is a resident</span>
+                            </li>
+                            <li class="point-circle"><span>the counterparty is a non-profit organization or state-financed organization and is going to provide goods / <span>services</span> worth UAH 93,075 UAH and more in the course of 2018.</span>
+                            </li>
+                        </ul>
+                        <p>
+                            <strong>In all such instances, adjusting controlled transaction’s price</strong> by the taxpayer and increasing the income up to a figure of 30% of
+                            the invoiced
+                            value of the transaction can serve as an alternative to documentation preparation. Traditionally, the revenues should be increased only with
+                            respect to very small transactions (those amounting up to UAH 150-200 thousand). Should transactions involving the counterparty exceed this amount,
+                            it would be cheaper to place an order for <strong>preparation of transfer pricing documentation.</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 contain-widget-align-r">
+                <div class="contain-widget">
+                    <div class="widget">
+                        <div class="widget__head">
+                            Categories
+                        </div>
+                        <div class="widget__menu">
+                            <ul>
+                                <li><a href="#">Articles</a></li>
+                                <li><a href="#">Business analytics</a></li>
+                                <li><a href="#">Transfer pricing</a></li>
+                                <li><a href="#">Career</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="contain-widget">
+                    <div class="widget">
+                        <div class="widget__head">
+                            blog tegs
+                        </div>
+                        <div class="widget__tag">
+                            <div class="widget__tag_a"><a href="#">Transfer pricing</a></div>
+                            <div class="widget__tag_a"><a href="#">Taxation</a></div>
+                            <div class="widget__tag_a"><a href="#">Control</a></div>
+                            <div class="widget__tag_a"><a href="#">2018</a></div>
+                            <div class="widget__tag_a"><a href="#">Money</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="contain-widget">
+                    <div class="widget">
+                        <div class="widget__tabs">
+                            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home"
+                                       aria-selected="true">popular</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile"
+                                       aria-selected="false">NEW</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="widget__news">
+                            <div class="tab-content" id="pills-tabContent">
+                                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                                    <div class="news">
+                                        <a href="#">
+                                            <div class="news__img"><img src="img/img_3.png" alt="img"></div>
+                                        </a>
+                                        <div class="contain__news_inf">
+                                            <div class="news__text">Comparison methods in transfer pricing</div>
+                                            <div class="news__data">Aprile 2018</div>
+                                        </div>
+                                    </div>
+                                    <div class="news">
+                                        <a href="#">
+                                            <div class="news__img"><img src="img/img_4.png" alt="img"></div>
+                                        </a>
+                                        <div class="contain__news_inf">
+                                            <div class="news__text">Which portion of the analytics should be ...</div>
+                                            <div class="news__data">Junuary 2018</div>
+                                        </div>
+                                    </div>
+                                    <div class="news">
+                                        <a href="#">
+                                            <div class="news__img"><img src="img/img_5.png" alt="img"></div>
+                                        </a>
+                                        <div class="contain__news_inf">
+                                            <div class="news__text">Contact center: luxury or necessity?</div>
+                                            <div class="news__data">Marth 2018</div>
+                                        </div>
+                                    </div>
+                                    <div class="news">
+                                        <a href="#">
+                                            <div class="news__img"><img src="img/img_6.png" alt="img"></div>
+                                        </a>
+                                        <div class="contain__news_inf">
+                                            <div class="news__text">Aim opens its academy</div>
+                                            <div class="news__data">Junuary 2018</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                                    <div class="news">
+                                        <a href="#">
+                                            <div class="news__img"><img src="img/img_3.png" alt="img"></div>
+                                        </a>
+                                        <div class="contain__news_inf">
+                                            <div class="news__text">Comparison methods in transfer pricing</div>
+                                            <div class="news__data">Aprile 2018</div>
+                                        </div>
+                                    </div>
+                                    <div class="news">
+                                        <a href="#">
+                                            <div class="news__img"><img src="img/img_5.png" alt="img"></div>
+                                        </a>
+                                        <div class="contain__news_inf">
+                                            <div class="news__text">Contact center: luxury or necessity?</div>
+                                            <div class="news__data">Marth 2018</div>
+                                        </div>
+                                    </div>
+                                    <div class="news">
+                                        <a href="#">
+                                            <div class="news__img"><img src="img/img_6.png" alt="img"></div>
+                                        </a>
+                                        <div class="contain__news_inf">
+                                            <div class="news__text">Aim opens its academy</div>
+                                            <div class="news__data">Junuary 2018</div>
+                                        </div>
+                                    </div>
+                                    <div class="news">
+                                        <a href="#">
+                                            <div class="news__img"><img src="img/img_4.png" alt="img"></div>
+                                        </a>
+                                        <div class="contain__news_inf">
+                                            <div class="news__text">Which portion of the analytics should be ...</div>
+                                            <div class="news__data">Junuary 2018</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="contain-banner">
+                    <div class="banner">
+                        <div class="banner__img"><img src="img/img_7.png" alt="img"></div>
+                        <div class="contain-banner-inf">
+                            <div class="banner__text">ЧИ ПІДПАДАЄ ВАША КОМПАНІЯ ПІД ТРАНСФЕРТНЕ ЦІНОУТВОРЕННЯ</div>
+                            <div class="banner__link"><a href="#">Пройти перевірку</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="contain-rate">
+                    <div class="rate">
+                        <div class="rate__text">
+                            Rate this post:
+                        </div>
+                        <div class="rate__star">
+                            <div class="rate__star_ico"><img src="img/star-red.svg" alt="ico"></div>
+                            <div class="rate__star_ico"><img src="img/star-red.svg" alt="ico"></div>
+                            <div class="rate__star_ico"><img src="img/star-red.svg" alt="ico"></div>
+                            <div class="rate__star_ico"><img src="img/star-red-white.svg" alt="ico"></div>
+                            <div class="rate__star_ico"><img src="img/star-white.svg" alt="ico"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="contain-social">
+                    <div class="social__text">
+                        Liked it Share in social networks:
+                    </div>
+                    <div class="social__link">
+                        <div class="social__link_a"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></div>
+                        <div class="social__link_a"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></div>
+                        <div class="social__link_a"><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></div>
+                        <div class="social__link_a"><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="appeal__section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="contain-appeal">
+                    <div class="appeal"><a href="#"><img class="img-fluid" src="img/img_8.png" alt="img"></a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/popper.min.js"></script>
